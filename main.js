4ca36512d16e3f8f612440d94287363bc003db75
Sumaiya Khan <sumaiya.khan32665@gmail.com>
2023-08-15 18:50:26 +0600

    added validation for email and password

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
 // step-1 :validate the password and email 
 
 //1.1-Add event on the button
-document.getElementById()
+document.getElementById('submit-btn').addEventListener('click',function(){
+    // 1.2--take the email and password field
+    const emailField=document.getElementById('correct-email');
+    const passwordField=document.getElementById('correct-password');
+    
+    // 1.3------get the value of email and password
+    const givenEmailValue=emailField.value;
+    const givenPasswordValue=passwordField.value;
+    //1.4 ----validate if the password and email is correct or not 
+    if(givenEmailValue==='sumu@gmail.com' && givenPasswordValue==='1234'){
+        console.log('gd');
+    }
+    else{
+        alert('Please enter the correct email and password');
+    }
+    // 1.5------clear the mail and password
+    emailField.value='';
+    passwordField.value='';
+
+
+})
