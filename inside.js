1b45c5bff564d3ecb8afdc96476e98f5ea400505
Sumaiya Khan <sumaiya.khan32665@gmail.com>
2023-08-15 21:52:15 +0600

    added withdraw

--- inside.js
+++ inside.js
@@ -1,11 +1,13 @@
 // 2--------Deposit money
 // 2.1----Add event on deposit button
 document.getElementById('deposit-btn').addEventListener('click',function(){
+    
     // 2.2 -----get the deposit input value
     const depositField=document.getElementById('deposit-value');
     const depositAmountString=depositField.value;
     //2.3-------convert string to number
-    const depositAmount=parseFloat(depositAmountString)
+    const depositAmount=parseFloat(depositAmountString);
+    
     //2.4-----get the preivous deposit by innertext
     const previousDeposit=document.getElementById('previous-deposit');
     // 2.5----get the preivous deposit 
@@ -25,6 +27,32 @@ document.getElementById('deposit-btn').addEventListener('click',function(){
     const newBalance=previousBalace+depositAmount;
     banalceField.innerText=newBalance;
     
+})
+
+//3--------withdraw
+//3.1---------get the button and apply event listener
+document.getElementById('withdraw').addEventListener('click',function(){
+    // 3.2 ------get the withdraw value
+    const withdrawValue=document.getElementById('withdraw-value');
+    const withdrawValueString=withdrawValue.value;
+    const withdraw=parseFloat(withdrawValueString);
+    
+    //3.3-----clear the feild
+    withdrawValue.value='';
+    //3.4---------get the previous withdraw
+    const previousWithdraw=document.getElementById('previous-withdraw');
+    const previousWithdrawString=previousWithdraw.innerText;
+    const previousWithdrawValue=parseFloat(previousWithdrawString);
+    //3.5-----------get the total withdraw
+    const recentWithdraw=withdraw+previousWithdrawValue;
+    //3.6-------set the value
+    previousWithdraw.innerText=recentWithdraw;
+    //3.7----------update tHE banalce
+    const banalceField=document.getElementById('balance');
+    const Totalbalance=banalceField.innerText;
+    const previousBalace=parseFloat(Totalbalance);
+    const newBalance=previousBalace-recentWithdraw;
+    banalceField.innerText=newBalance;
 
 
 })
